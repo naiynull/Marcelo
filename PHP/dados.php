f3df7f8e737cffae7ff4d005323c6aa581c2b68f
naiynull <manuborges011@gmail.com>
2024-02-27 15:32:54 -0300

    1

---
+++ PHP/dados.php
@@ -0,0 +1,10 @@
+<?php
+
+$dados[] = array("Nome" => "Luís Coco pelado", "Idade" => 24, "Estilo" => "kpop");
+$dados[] = array("Nome" => "Lilice Matias", "Idade" => 63, "Estilo" => "brega Funk");
+$dados[] = array("Nome" => "Emanu Linda", "Idade" => 17, "Estilo" => "rock");
+$dados[] = array("Nome" => "gusta souza", "Idade" => 24, "Estilo" => "rock");
+$dados[] = array("Nome" => "ferr neto", "Idade" => 23, "Estilo" => "kpop");
+$dados[] = array("Nome" => "malu Rufino", "Idade" => 18, "Estilo" => "rock");
+
+?>
---
+++ gostarock.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilo.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <title>Aplicações Web II</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                <a class="nav-link active" href="./index.html">Inicio
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> </a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <p class="h1 text-center">Manipulação de Arrays em PHP </p>
+            <p class="h9 text-center">Exercício de utilização de arrays em PHP </p>
+        </div>
